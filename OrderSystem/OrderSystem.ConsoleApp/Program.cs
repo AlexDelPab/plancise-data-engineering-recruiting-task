cd6e4d8fc4f8fbc276df6aa4ac15275d6a104aa5
Roman Sekot <roman@plansice.site>
2023-01-24 15:42:04 +0100

    feature: populate database with random orders

--- OrderSystem/OrderSystem.ConsoleApp/Program.cs
+++ OrderSystem/OrderSystem.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using OrderSystem.ConsoleApp.Data.Context;
@@ -31,13 +32,47 @@ public class Program
 
     private static void Populate(OrderSystemDbContext dbContext)
     {
+        var employees = new string[]
+        {
+            "Harriett Hensley",
+            "Foster Stevenson",
+            "Suarez McFadden",
+            "Graham Alston",
+            "Graham A.",
+            "Brooke Powell",
+            "5d9dd7ad-a643-4d31-ba76-fc21713e2709",
+            "871a6ef0-b9cd-481e-9bad-1320322dc2e5",
+            "Roberts Hendrix",
+            "Leon Rivas",
+            "Smith Frederick",
+            "Freda Norton",
+            "be6ec6a6-f75b-41a0-83b0-81c397d28859",
+            "be6ec6a6-f75b-41a0-83b0-81c397d26f59",
+            "63cfe6efdc51fe6ee2b21a29",
+            "63cfe6ef1866dfbf1957ae8f",
+            "Hardin Noble",
+            "Hardin N.",
+            "H.N.",
+            "Toni Villarreal",
+            "Stefan Ebelsberg",
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            string.Empty
+        };
+
+        var rnd = new Random();
         while(true)
         {
             Thread.Sleep(2000);
+
+            var nameIndex = rnd.Next(employees.Length - 1);
+            var name = employees[nameIndex];
+            
             var order = new OrderEntity
             {
                 Id = Guid.NewGuid(),
-                AssignedEmployee = "Stefan M.",
+                AssignedEmployee = name,
                 Date = DateTime.Now,
                 Notes = "Order due tomorrow",
                 OrderedItem = Guid.NewGuid()
